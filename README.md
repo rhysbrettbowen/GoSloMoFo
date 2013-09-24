1dedb2679125d19b1e7b88e21db11c51b657df44
Rhys <rhys@dataminr.com>
2013-09-24 10:31:42 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#GoSloMoFo
+
+An experiment in trying to slow down the frame rate of a page
---
+++ goslomofo.js
@@ -0,0 +1,45 @@
+window.requestAnimationFrame = (function(){
+    return  window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.requestAnimFrame ||
+        function(callback){
+	        window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
+var goslomofo = function(fn, fr) {
+
+	var running = false;
+
+	var fr = fr || 30;
+	var ms = 1000 / fr;
+	var last;
+
+	var go = function() {
+		if (!running) {
+			return;
+		}
+		if (last) {
+			while (+(new Date()) - last < ms) {
+				var div = document.createElement('div');
+				document.body.appendChild(div);
+				window.getComputedStyle(div);
+				document.body.removeChild(div);
+			}
+		}
+		last = new Date();
+		fn();
+		requestAnimationFrame(go);
+	};
+
+	return {
+		start: function() {
+			running = true;
+			requestAnimationFrame(go);
+		},
+		stop: function() {
+			running = false;
+		}
+	};
+};
